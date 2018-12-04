4a7678e6faeef3d3e75607116f8a77e49b776cd9
Brady <brady.withers@outlook.com>
2018-12-03 21:12:30 -0700

    comments on code

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
 
+
+	// TOUCHSCREEN AND MOBILE PHONE SUPPORT FOR HIGHLIGHT
 $(".title").on("touchstart", function () {
-	$(".title").css("background-position", "-100% 0")
+	$(".title").css("background-position", "-100% 0");
 });
 $(".title").on("touchend", function () {
-	$(".title").css("background-position", "0 0")
+	$(".title").css("background-position", "0 0");
 });
+//END OF TOUCHSCREEN AND MOBILE PHONE SUPPORT
+
 
 });
