148b363d64a9366a0d8d840a266d636ac2cc52b2
Brady <brady.withers@outlook.com>
2019-05-15 20:38:06 -0600

    moved url script to JS file, code cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,22 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
 
 	// TOUCHSCREEN AND MOBILE PHONE SUPPORT FOR HIGHLIGHT
-$(".title").on("touchstart", function () {
-	$(".title").css("background-position", "-100% 0");
-});
-$(".title").on("touchend", function () {
-	$(".title").css("background-position", "0 0");
-});
-//END OF TOUCHSCREEN AND MOBILE PHONE SUPPORT
+	$(".title").on("touchstart", function () {
+		$(".title").css("background-position", "-100% 0");
+	});
+	$(".title").on("touchend", function () {
+		$(".title").css("background-position", "0 0");
+	});
+	//END OF TOUCHSCREEN AND MOBILE PHONE SUPPORT
 
+	//start of script to capture and use the URL to change the body color
+	var urlParams = new URLSearchParams(window.location.search);
+	console.log(urlParams.has('red'));
+	if (urlParams.has('red')) {
+		console.log('red is working');
+		$('body').css('background', 'red');
+	};
+	//end of script to capture and use the URL to change the body color
 
 });
